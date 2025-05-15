0797d5f6d46570340828a7697fe335e45b4ed942
Resha1231213 <ishmaevrishat97@gmail.com>
2025-05-15 23:51:22 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from diagnosis.views import submit_request, premium_result
+from diagnosis.views import submit_request, result_view, premium_result_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', submit_request, name='submit_request'),
-    path('premium/<int:request_id>/', premium_result, name='premium_result'),
-    path('premium/', views.premium_result, name='premium_result'),
+    path('', submit_request, name='submit'),
+    path('result/', result_view, name='result'),
+    path('premium/', premium_result_view, name='premium_result'),
 ]
