d2872b6b2f941a8dd33984ac0a79f9d9f5a307c5
Resha1231213 <ishmaevrishat97@gmail.com>
2025-05-15 23:47:14 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', submit_request, name='submit_request'),
     path('premium/<int:request_id>/', premium_result, name='premium_result'),
+    path('premium/', views.premium_result, name='premium_result'),
 ]
