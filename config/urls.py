13d3065313908ab71fc6f2397604e346803385bc
Resha1231213 <ishmaevrishat97@gmail.com>
2025-05-15 23:40:07 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from diagnosis.views import submit_request  # <-- добавить
+from diagnosis.views import submit_request, premium_result
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', submit_request, name='submit_request'),  # <-- это твоя форма на главной
+    path('', submit_request, name='submit_request'),
+    path('premium/<int:request_id>/', premium_result, name='premium_result'),
 ]
